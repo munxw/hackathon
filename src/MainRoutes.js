dfe20cbfee07a47e4773e3fc3e8f6301779722bc
unknown <orozbekova1488@gmail.com>
2023-01-31 13:12:33 +0600

    Navbar

---
+++ src/MainRoutes.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ContactPages from "./pages/ContactPages";
+
+const MainRoutes = () => {
+  const PUBLIC_ROUTES = [
+    { link: "/", element: <HomePage />, id: 1 },
+    { link: "/auth", element: <AuthPage />, id: 2 },
+    { link: "*", element: <NotFounedPage />, id: 5 },
+    { link: "/products", element: <ProductPage />, id: 3 },
+    { link: "/products/:id", element: <ProductDetailsPage />, id: 4 },
+    { link: "/contactus", element: <ContactPages />, id: 6 },
+    { link: "/aboutus", element: <AboutUsPage />, id: 7 },
+    { link: "/cart", element: <CartPage />, id: 8 },
+  ];
+  return (
+    <div>
+      <Routes>
+        {PUBLIC_ROUTES.map((item) => (
+          <Route path={item.link} element={item.element} key={item.id} />
+        ))}
+      </Routes>
+    </div>
+  );
+};
+
+export default MainRoutes;
