4e33785a263c4d7483aaf8f35a347f623f4af9c4
saadat04 <saadatik88@gmail.com>
2023-02-02 14:52:46 -0800

    commit about us

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 import MainRouts from "./MainRouts";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <MainRouts />
     </div>
   );
--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../AboutUs/About.css";
-import { Link } from "react-router-dom";
 
 const AboutUs = () => {
   return (
